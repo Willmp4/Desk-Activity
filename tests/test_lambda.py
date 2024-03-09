797ae97aae3cdfe31fd4eb9a27de92076128e24b
Willmp4 <54776440+Willmp4@users.noreply.github.com>
2024-03-09 12:02:27 +0000

    Refactor test_invoke_lambda_with_test_data function in test_lambda.py

--- tests/test_lambda.py
+++ tests/test_lambda.py
@@ -11,26 +11,28 @@ def lambda_client():
 
 def test_invoke_lambda_with_test_data(lambda_client):
     test_data = {
-        "user_id": "erykah",
-        "events": [
-            {
-                "timestamp": "2024-03-07T12:00:00",
-                "type": "keyboard_activity_session",
-                "data": {
-                    "start_time": "2024-03-07T12:00:00",
-                    "end_time": "2024-03-07T12:01:00",
-                    "key_strokes": 50
+        "body":json.dumps({
+            "user_id": "erykah",
+            "events": [
+                {
+                    "timestamp": "2024-03-07T12:00:00",
+                    "type": "keyboard_activity_session",
+                    "data": {
+                        "start_time": "2024-03-07T12:00:00",
+                        "end_time": "2024-03-07T12:01:00",
+                        "key_strokes": 50
+                    }
+                },
+                {
+                    "timestamp": "2024-03-07T12:05:00",
+                    "type": "mouse_movement",
+                    "data": {
+                        "start_position": (100, 100),
+                        "end_position": (200, 200)
+                    }
                 }
-            },
-            {
-                "timestamp": "2024-03-07T12:05:00",
-                "type": "mouse_movement",
-                "data": {
-                    "start_position": (100, 100),
-                    "end_position": (200, 200)
-                }
-            }
-        ]
+            ]
+        })
     }
 
     try:
@@ -42,7 +44,8 @@ def test_invoke_lambda_with_test_data(lambda_client):
 
         # Decode the response payload
         response_payload = json.load(response['Payload'])
-        assert 'Data uploaded successfully to S3' in response_payload['body']
         print("Lambda invocation response:", response_payload)
+        assert 'Data uploaded successfully' in response_payload['body']
+
     except Exception as e:
         pytest.fail(f"Error invoking Lambda function: {e}")
